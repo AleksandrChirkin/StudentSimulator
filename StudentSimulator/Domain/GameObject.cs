61bb567a794d8e2df68b6adced9cca7869e52d6c
Titov Stepan <titovstepan@mail.ru>
2021-04-28 23:58:29 +0500

    add some logic in player and task

--- StudentSimulator/Domain/GameObject.cs
+++ StudentSimulator/Domain/GameObject.cs
@@ -1,8 +1,27 @@
-﻿namespace StudentSimulator.Domain
+﻿using System.Collections.Generic;
+
+namespace StudentSimulator.Domain
 {
     public class GameObject
     {
         public string Name { get; }
-        public Task Task { get; }
+        private List<Task> tasks = new List<Task>();
+
+        public GameObject(string name, params Task[] tasks)
+        {
+            Name = name;
+            this.tasks.AddRange(tasks);
+        }
+
+        private IEnumerable<Task> GetTasks()
+        {
+            foreach (var task in tasks)
+                yield return task;
+        }
+
+        public Task GetNextTask()
+        {
+            return GetTasks().GetEnumerator().Current;
+        }
     }
 }
--- StudentSimulator/Domain/Player.cs
+++ StudentSimulator/Domain/Player.cs
@@ -1,21 +1,32 @@
-﻿namespace StudentSimulator.Domain
+﻿using System.Collections.Generic;
+
+namespace StudentSimulator.Domain
 {
     public class Player
     {
         public string Name { get; }
-        public int Expulsion { get; } // отчисление
-        public int Time { get; }
-        public int Experience { get; }
-        public int AlgebraSkill { get; private set; }
-        public int ProgrammingSkill { get; private set; }
-        public int PhilosophySkill { get; private set; }
+        public StatParameters Stats { get; private set; }
+        public ISkillParameters AlgebraSkill { get; private set; }
+        public ISkillParameters ProgrammingSkill { get; private set; }
+        public ISkillParameters PhilosophySkill { get; private set; }
         public int Authority { get; private set; }
+
+        private readonly List<Task> tasks = new List<Task>();
         // всё что выше, нужно лучше проработать и понять как игрок будет знать про свои скиллы.
 
         public void InteractWith(GameObject gameObject)
         {
-
+            tasks.Add(gameObject.GetNextTask());
         }
 
+        public void MakeTask(Task task)
+        {
+            Stats = new StatParameters(
+                Stats.Expulsion + task.Prices.Expulsion,
+                Stats.Time + task.Prices.Time,
+                Stats.Energy + task.Prices.Energy,
+                Stats.Experience + task.Prices.Experience);
+            
+        }
     }
 }
